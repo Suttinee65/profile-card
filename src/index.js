2f37f2d0787b1e777a2c5422ade458dbe71514fc
Suttinee65 <suttinee13487@gmail.com>
2023-07-03 23:32:51 +0700

    Add message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+    <div className='card'>
+      <Avatar />
+      <div className='data'>
+        <Intro />
+        <SkillList />
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+function SkillList() {
+  return (
+    <div className='skill-list'>
+      <Skill skill="PYTHON" emoji="🍊" color="cream" />
+      <Skill skill="REACT" emoji="🌷" color="cream" />
+      <Skill skill="ARRAY" emoji="🥖" color="cream" />
+    </div>
+  )
+}
+
+function Skill(props) {
+  return (
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  )
+}
+
+function Intro() {
+  return <div>
+    <h1>นางสาวสุทธิณี สาริก</h1>
+    <p>
+      นักศึกษาสาขาเทคโนโลยีสารสนเทศและการสื่อสาร
+      คณะวิทยาศาสตร์ มหาวิทยาอุบลราชธานี
+      ชื่อนชอบในการออกแบบ วาดภาพโดยใช้คอมพิวเตอร์
+      งานอดิเรก คือ อ่านหนังสือ ดูหนัง ซีรีส์ ฟังเพลง เล่นเกมและนอน
+    </p>
+  </div>
+}
+
+function Avatar() {
+  return <img className='avatar' src='my.jpg' alt='My Avatar' />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +59,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
